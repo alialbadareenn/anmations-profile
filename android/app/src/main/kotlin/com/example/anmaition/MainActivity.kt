d27b5f3f936126dea4f0a8b2bda05fc422e42dfe
alialbadareenn <101819933+alialbadareenn@users.noreply.github.com>
2022-03-17 19:40:23 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/anmaition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anmaition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\abadareen\\Downloads\\flutter_windows_2.10.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\abadareen\\Desktop\\anmaition" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\abadareen\\Downloads\\flutter_windows_2.10.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\abadareen\\Desktop\\anmaition"
+  "FLUTTER_ROOT=C:\\Users\\abadareen\\Downloads\\flutter_windows_2.10.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\abadareen\\Desktop\\anmaition\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\abadareen\\Desktop\\anmaition"
+  "FLUTTER_TARGET=C:\\Users\\abadareen\\Desktop\\anmaition\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\abadareen\\Desktop\\anmaition\\.dart_tool\\package_config.json"
+)
